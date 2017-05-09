7586f5ee6a3d86483e9dfd951508c6c08d955948
xiaoqixuan <212538776@qq.com>
2017-05-09 11:49:38 +0800

    add PrimeNumberQuestion

---
+++ src/main/java/com.worldly.logic/PrimeNumberQuestion.java
@@ -0,0 +1,37 @@
+package com.worldly.logic;
+
+/**
+ * 题目：判断101-200之间有多少个素数，并输出所有素数。程序分析：
+ * 判断素数的方法：如果一个数只能被1和本身整除才是素数，不然就不是。
+ * @author xiaoqixuan
+ * @create 2017/5/9 9:43
+ */
+public class PrimeNumberQuestion {
+    public static void main(String[]args){
+        boolean flag = true;
+        for(int i=101;i<=200;i++){
+           flag=isPrime(i);
+           if(flag) {
+               System.out.println(i + "是素数");
+           }else{
+               System.out.println(i+"不是素数");
+           }
+        }
+    }
+
+    private static boolean isPrime(int number){
+        boolean flag = false;
+        if(number<1){
+            return  flag;
+        } else{
+            for(int i=2;i<number;i++){
+                if(number%i==0){
+                    flag=false;
+                    break;
+                }
+                flag = true;
+            }
+        }
+        return flag;
+    }
+}
