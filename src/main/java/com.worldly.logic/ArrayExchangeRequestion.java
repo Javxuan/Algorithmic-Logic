4b9d279befcd4ff3a7cf074c6f709ceb8de12f4c
xiaoqixuan <212538776@qq.com>
2017-05-11 10:47:16 +0800

    add 数组逆序问题

---
+++ src/main/java/com.worldly.logic/ArrayExchangeRequestion.java
@@ -0,0 +1,35 @@
+package com.ht.report.controller;
+
+/**
+ * 将一个数组逆序输出。
+ * 程序分析：用第一个与最后一个交换。
+ * @author xiaoqixuan
+ * @create 2017/5/11 9:22
+ */
+public class ArrayExchangeRequestion {
+    public static void main(String []args){
+        int [] arr ={1,12,31,44,5,61,7,81,9,10};
+        ArrayExchangeRequestion arrayExchangeRequestion = new ArrayExchangeRequestion();
+        arrayExchangeRequestion.show(arr);
+        arrayExchangeRequestion.exchage(arr);
+        System.out.println("\n"+"---------后------------");
+        arrayExchangeRequestion.show(arr);
+    }
+
+    public void exchage(int[] arr){
+        int temp=0;
+        //要确定分界值
+       for(int i=0;i<arr.length/2;i++){
+           temp = arr[i];
+           arr[i]=arr[arr.length-i-1];
+           arr[arr.length-i-1]=temp;
+       }
+    }
+
+    public void show(int[]arr){
+        for(int a:arr){
+            System.out.print(a+" ");
+        }
+    }
+
+}
